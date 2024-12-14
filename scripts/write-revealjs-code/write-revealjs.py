2cc15d90156006f1eab60451afc767ff3bffa31e
venugvgk <venu.gvgk@gmail.com>
2024-12-14 13:47:47 +0530

    added treekar telugu book

--- scripts/write-revealjs-code/write-revealjs.py
+++ scripts/write-revealjs-code/write-revealjs.py
@@ -1,11 +1,11 @@
 import os
 from natsort import os_sorted
-path= "C:/Users/Venu/Documents/GitHub/venugvgk.github.io/books/own-picture-books/treekar-shomus"
+path= "C:/Users/Venu/Documents/GitHub/venugvgk.github.io/books/own-picture-books/treekar-shomus/treekar-shomus-telugu"
 file_names = os_sorted(os.listdir(path))
 print(file_names)
 f = open("reveal.txt", "w")
 for name in file_names:
     f = open("reveal.txt", "a")
-    f.write("\n##\n\n![]" + name + "\n")
+    f.write("\n##\n\n![]" + "("+name+")" + "\n")
 f.close()
 
